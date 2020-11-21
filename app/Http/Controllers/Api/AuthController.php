aeb50c958942c00e74c90845e7bfc67361d7683e
MarwanSallum <maslloum2091@gmail.com>
2020-11-21 12:25:10 +0300

    add user id to respond token when login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -42,6 +42,7 @@ class AuthController extends Controller
     }
     protected function respondWithToken($token){
       return response()->json([
+        'id' => auth()->user()->id,
         'role' => auth()->user()->roles->first()->name,
         'access_token' => $token,
         'token_type' => 'bearer',
